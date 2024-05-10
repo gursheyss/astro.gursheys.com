e0236e2b880671f9d32a547c154cdd0ead5fa757
gursheyss <rkwarya@gmail.com>
2024-05-09 22:59:57 -0700

    start w blog

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,32 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import svelte from "@astrojs/svelte";
-
+import remarkSmartypants from "remark-smartypants";
 import vercel from "@astrojs/vercel/serverless";
+import rehypePrettyCode from "rehype-pretty-code";
+import theme from "./syntax-theme.json";
+
+import mdx from "@astrojs/mdx";
+
+const prettyCodeOptions = {
+  theme,
+  onVisitHighlightedLine(node) {
+    node?.properties?.className
+      ? node.properties.className.push("highlighted")
+      : (node.properties.className = ["highlighted"]);
+  },
+  onVisitHighlightedChars(node) {
+    console.log(node);
+    node?.properties?.className
+      ? node.properties.className.push("highlighted-chars")
+      : (node.properties.className = ["highlighted-chars"]);
+  },
+  tokensMap: {},
+};
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), svelte()],
+  integrations: [tailwind(), svelte(), mdx()],
   output: "server",
   adapter: vercel({
     isr: {
@@ -17,4 +37,12 @@ export default defineConfig({
       enabled: true,
     },
   }),
+  markdown: {
+    syntaxHighlight: false,
+    remarkPlugins: [[remarkSmartypants, {}]],
+    rehypePlugins: [[rehypePrettyCode, prettyCodeOptions]],
+    shikiConfig: {
+      theme,
+    },
+  },
 });
