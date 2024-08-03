0003f2ac1e3bc5da8e067fd92bbb1d8436af1170
gursheyss <rkwarya@gmail.com>
2024-08-03 15:13:47 -0700

    fix now playing

--- src/pages/api/now-playing.ts
+++ src/pages/api/now-playing.ts
@@ -3,6 +3,5 @@ import { getNowPlaying } from "../../lib/spotify";
 
 export const GET: APIRoute = async () => {
   const spotifyData = await getNowPlaying();
-
   return new Response(JSON.stringify(spotifyData));
 };
