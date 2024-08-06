016d4d34df1227a1bceab02227623e74197972ed
gursheyss <rkwarya@gmail.com>
2024-08-06 12:50:44 -0700

    more stuff

---
+++ src/components/Lightswitch.tsx
@@ -0,0 +1,57 @@
+import React, { useState, useEffect } from "react";
+import { motion, AnimatePresence } from "framer-motion";
+
+export const Lightswitch = () => {
+  const [imgSrc, setImgSrc] = useState("/moon.png");
+
+  const switchImage = () => {
+    setImgSrc((prevSrc) =>
+      prevSrc === "/moon.png" ? "/sun.png" : "/moon.png"
+    );
+  };
+
+  useEffect(() => {
+    document.body.className = imgSrc === "/moon.png" ? "moon" : "sun";
+  }, [imgSrc]);
+
+  const variants = {
+    initial: { y: -20, opacity: 0, rotate: -45 },
+    animate: { y: 0, opacity: 1, rotate: 0 },
+    exit: { y: -20, opacity: 0, rotate: 45 },
+    hover: { scale: 1.1 },
+  };
+
+  return (
+    <AnimatePresence mode="wait">
+      <motion.img
+        key={imgSrc}
+        src={imgSrc}
+        alt={imgSrc === "/moon.png" ? "moon" : "sun"}
+        width={24}
+        height={24}
+        className="pixelated inline-block align-middle cursor-pointer"
+        onClick={switchImage}
+        variants={variants}
+        initial="initial"
+        animate="animate"
+        exit="exit"
+        transition={{
+          type: "spring",
+          stiffness: 260,
+          damping: 20,
+          duration: 0.5,
+        }}
+        whileHover={{
+          scale: 1.1,
+          rotate: [0, -10, 0, 10, 0],
+          transition: {
+            rotate: {
+              repeat: Infinity,
+              duration: 0.5,
+            },
+          },
+        }}
+      />
+    </AnimatePresence>
+  );
+};
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,10 +1,15 @@
 /** @type {import('tailwindcss').Config} */
+const vignetteZIndex = 10;
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     extend: {
       colors: {
         pink: "rgb(253, 207, 243)",
+      },
+      zIndex: {
+        vignette: `${vignetteZIndex}`,
+        aboveVignette: `${vignetteZIndex + 1}`,
       },
     },
   },
