545cd0a379720dad13d41fca815bc22061c8e1aa
gursheyss <rkwarya@gmail.com>
2024-08-05 21:37:31 -0700

    setups

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,14 @@
 import { defineConfig } from 'astro/config';
+import react from "@astrojs/react";
+
+import tailwind from "@astrojs/tailwind";
+import expressiveCode from "astro-expressive-code";
+import mdx from "@astrojs/mdx";
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [react(), tailwind(), expressiveCode(), mdx()],
+  output: "server",
+  adapter: vercel()
+});
